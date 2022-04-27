51c2c3fce25e601ff7feb6559389ea78d2b1950d
ashish923 <71844277+ashish923@users.noreply.github.com>
2022-04-27 17:48:24 +0530

    Time: 34 ms (5.51%), Space: 17.2 MB (97.71%) - LeetHub

---
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        
+        int res = 1;
+        queue<pair<TreeNode*, int>> q;
+        
+        // I am using intialising list
+        q.push({root, 0});      // also can use make_pair
+        int z = 0;
+        while(!q.empty())
+        {
+            int cnt = q.size();
+            // start is the index of root node for first level
+            int start = q.front().second;
+            int end = q.back().second;
+            cout<<res<<"\n";
+            // z = end;
+            res = max(res,end-start + 1);
+            cout<<start<<" "<<end<<"\n";
+            for(int i = 0; i <cnt; ++i)
+            {
+                pair<TreeNode*, int> p = q.front();
+                // we will use it while inserting it children
+                // left child will be 2 * idx + 1;
+                // right chils will be 2 * idx + 2;
+                int idx = p.second - start;
+                cout<<idx<<" ";
+                q.pop();
+                
+                // if  left child exist
+                if(p.first->left != NULL)
+                    q.push({p.first->left, (long long)2 * idx + 1});
+                
+                // if right child exist
+                if(p.first->right != NULL)
+                    q.push({p.first->right, (long long) 2 * idx + 2});
+            }
+            cout<<"\n";
+        }
+        
+        return res;
+        
+    }
+};
