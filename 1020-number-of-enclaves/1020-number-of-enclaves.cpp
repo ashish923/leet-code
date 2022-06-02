1873e9e0f780b92ad3e78dd7638eaa0aee331e3f
ashish923 <71844277+ashish923@users.noreply.github.com>
2022-06-02 23:54:42 +0530

    Time: 76 ms (63.59%), Space: 21.7 MB (86.46%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -62,7 +62,7 @@ public:
         if(i<0 || i>=n || j<0 || j>=m || grid[i][j] == 0) return;
         
         
-        if(grid[i][j] == 1) { count++;grid[i][j] = 0;}
+        grid[i][j] =0;
         dfs(grid, i - 1, j , n , m);
         dfs(grid, i + 1, j , n , m);
         dfs(grid, i, j - 1 , n , m);
