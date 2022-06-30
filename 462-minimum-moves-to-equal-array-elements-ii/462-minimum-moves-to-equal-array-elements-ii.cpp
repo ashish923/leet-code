844a1b00047ec1ecd550dd88f4511b590d1a9cac
ashish923 <71844277+ashish923@users.noreply.github.com>
2022-06-30 18:36:17 +0530

    Time: 21 ms (26.38%), Space: 10.9 MB (59.74%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -4,15 +4,9 @@ public:
         sort(nums.begin(),nums.end());
         int s=INT_MAX,l = INT_MIN,n = nums.size(); 
         int mid = nums[n/2];
-        cout<<mid<<" ";
         int sum = 0;
         for(int i = 0; i < n; i++){
-            
-            
             sum += abs(nums[i] - mid);
-            
-            // if(nums[i] > mid) sum += nums[i] - mid;
-            // else if(nums[i] < mid) sum += mid - nums[i];
             
         }
         
