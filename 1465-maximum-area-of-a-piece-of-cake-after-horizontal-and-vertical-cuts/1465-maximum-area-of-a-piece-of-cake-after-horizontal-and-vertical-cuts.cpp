1cd51d6b6af5f85439840aeac1acd851b9f95ed3
ashish923 <71844277+ashish923@users.noreply.github.com>
2022-07-02 14:29:25 +0530

    Time: 131 ms (20.93%), Space: 34.7 MB (6.04%) - LeetHub

---
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
+        
+        sort(horizontalCuts.begin(),horizontalCuts.end());
+        sort(verticalCuts.begin(),verticalCuts.end());
+        
+        
+        vector<int> height{horizontalCuts[0]};
+        vector<int> length{verticalCuts[0]};
+        
+        for(int i=1; i<horizontalCuts.size(); i++){
+            height.push_back(horizontalCuts[i] - horizontalCuts[i-1]);
+        }
+        height.push_back(h - horizontalCuts[horizontalCuts.size()-1]);
+        
+        for(int i=1; i<verticalCuts.size(); i++){
+            length.push_back(verticalCuts[i] - verticalCuts[i-1]);
+        }
+        length.push_back(w - verticalCuts[verticalCuts.size() - 1]);
+        
+        long long int a = *max_element(height.begin(), height.end());
+        long long int b = *max_element(length.begin(), length.end());
+        cout<<a<<" "<<b<<" ";
+        int mod = 1000000007;
+        // return (int)(a%mod*b%mod);
+        a = (int)a%mod;
+        b = (int)b%mod;
+        cout<<a<<" "<<b<<" ";
+        
+        return (a*b)%mod;
+        
+        
+    }
+};
