956f488f1021d1d4197dab165b7c8b9334502073
ashish923 <71844277+ashish923@users.noreply.github.com>
2022-07-05 20:02:38 +0530

    Time: 52 ms (26.54%), Space: 12.3 MB (17.73%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -3,11 +3,23 @@ public:
     int minDistance(string word1, string word2) {
         
    
-        int m = word1.length();
-        int n = word2.length();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        int z = solve(0,0,word1,word2,dp);
-        int k = m + n - z*2;
+        int n = word1.length();
+        int m = word2.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1));
+        for(int i=0; i<m; i++) dp[0][i] = 0;
+        for(int i=0; i<n; i++) dp[i][0] = 0;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                 if(word1[i - 1] == word2[j - 1])
+                    dp[i][j] =  1 + dp[i-1][j-1];
+        
+                else
+                    dp[i][j] =  max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        // int z = solve(n,m,word1,word2,dp);
+        int k = m + n - dp[n][m]*2;
         return k;
         
     }
@@ -15,15 +27,15 @@ public:
     
     int solve(int i,int j,string s1,string s2,vector<vector<int>>& dp){
         
-        if(i == s1.length() || j == s2.length())
+        if(i == 0 || j == 0)
             return 0;
         if(dp[i][j] != -1)
             return dp[i][j];
-        if(s1[i] == s2[j]){
-            return dp[i][j] =  1 + solve(i+1,j+1,s1,s2,dp);
-        }
+        if(s1[i - 1] == s2[j - 1])
+            return dp[i][j] =  1 + solve(i-1,j-1,s1,s2,dp);
+        
         else
-            return dp[i][j] =  max(solve(i+1,j,s1,s2,dp),solve(i,j+1,s1,s2,dp));
+            return dp[i][j] =  max(solve(i-1,j,s1,s2,dp),solve(i,j-1,s1,s2,dp));
     }
     
 };
