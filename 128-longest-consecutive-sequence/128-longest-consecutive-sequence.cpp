d511015743fe22ff46c5a7f11853e71984c3de8b
ashish923 <71844277+ashish923@users.noreply.github.com>
2022-07-05 08:57:13 +0530

    Time: 177 ms (70.91%), Space: 49.7 MB (61.51%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -13,15 +13,18 @@ public:
         for(int i=0; i<n; i++){
             int z = nums[i];
             int c = 1;
+//             first we check ele is present or not 
             if(mp.find(z) != mp.end())
             {
                 int zm = z - 1;
+//          in this loop we r checking to left the side how many element are there consecutively
                 while(mp.count(zm) > 0){
                     mp.erase(zm);
                     zm--;
                     c++;
                 }
                 zm = z+1;
+//                 same procedure in right side
                while(mp.count(zm) > 0){
                     mp.erase(zm);
                     zm++;
